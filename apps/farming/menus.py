13ec787f7adedcaf681e3658aa97cde100a04245
Tovia <tobiasesteban5@gmail.com>
2022-02-28 17:46:58 -0300

    soporte menu sub menu

---
+++ apps/farming/menus.py
@@ -0,0 +1,13 @@
+from menu import Menu, MenuItem
+from django.shortcuts import reverse
+
+
+children = (
+    MenuItem(
+        "Finca",
+        reverse("finca_list"),
+        icon="fa fa-map",        
+    ),
+)
+
+Menu.add_item("agricultura", MenuItem("Agricultura", reverse(),icon='fa fa-tractor',weight=6, children=children))
