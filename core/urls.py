0ef4bd8c5ab0ca2d92960ae8ece3da5a800c083b
Tovia <tobiasesteban5@gmail.com>
2022-03-06 19:49:31 -0300

    agregar suministros

--- core/urls.py
+++ core/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [         # Django admin route
     path("farming/", include("apps.farming.urls")),             # UI Kits Html files
     path("inventory/", include("apps.inventory.urls")),             # UI Kits Html files
     path("finance/", include("apps.finance.urls")),             # UI Kits Html files
+    path("supplies/", include("apps.supplies.urls")),             # UI Kits Html files
     path("", include("apps.home.urls")),             # UI Kits Html files
 ]
