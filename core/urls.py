e23df3708562e1ceba2f277545b9d204fea4fd73
Tovia <tobiasesteban5@gmail.com>
2022-03-06 15:43:26 -0300

    agregar app inventory

--- core/urls.py
+++ core/urls.py
@@ -10,6 +10,7 @@ from django.conf.urls import url
 urlpatterns = [         # Django admin route
     path("", include("apps.authentication.urls")),
     path("farming/", include("apps.farming.urls")),             # UI Kits Html files
-    path("supplies/", include("apps.supplies.urls")),             # UI Kits Html files
+    path("inventory/", include("apps.inventory.urls")),             # UI Kits Html files
+    path("finance/", include("apps.finance.urls")),             # UI Kits Html files
     path("", include("apps.home.urls")),             # UI Kits Html files
 ]
