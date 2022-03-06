198b6cdc1ae1b76cfc081bfda9874ef468758294
Tovia <tobiasesteban5@gmail.com>
2022-03-06 15:39:33 -0300

    update user menu item icon

--- apps/authentication/menus.py
+++ apps/authentication/menus.py
@@ -3,4 +3,4 @@ from django.shortcuts import reverse
 
 
 
-Menu.add_item("usuarios", MenuItem("Usuarios", reverse("user_list"),icon='fas fa-users',weight=6))
+Menu.add_item("usuarios", MenuItem("Usuarios", reverse("user_list"),icon='fas fa-user-group',weight=6))
