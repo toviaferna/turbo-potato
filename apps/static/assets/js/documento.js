0ba41335c373a2b9667e141c18b69b77963f8a46
tobiasferna <tobiasesteban5@gmail.com>
2022-09-20 10:58:25 -0400

    Agregar consulta ruc

--- apps/static/assets/js/documento.js
+++ apps/static/assets/js/documento.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
     $("#id_documento").blur(function(){
         if($.trim($(this).val()).length > 0){
-            $.get("https://ruc.com.py/v2/getci/"+$(this).val(), function(data){
-                if(data.result){
-                    $("#id_razon_social").val(data.nombres+" "+data.apellidos)
-                }
-            });
+
+            if($(this).val().indexOf("-")>=0){ 
+                $.get("https://ruc.com.py/v2/get/"+$(this).val(), function(data){
+                    if(data.result){
+                        $("#id_razon_social").val(data.razon_social);
+                    }
+                });
+            }else{
+                $.get("https://ruc.com.py/v2/getci/"+$(this).val(), function(data){
+                    if(data.result){
+                        $("#id_razon_social").val(data.nombres+" "+data.apellidos);
+                    }
+                });
+            }
+
+            
         }
     });
 });
